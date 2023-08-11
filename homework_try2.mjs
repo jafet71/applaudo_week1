17f5e4a9f6efd2cf473ca320b56666cc5e03cd26
jafet.71 <jafetsalazar71@gmail.com>
2023-08-11 17:48:43 -0600

    Se corrigió la Funcionalidad de Reverse y Count

--- homework_try2.mjs
+++ homework_try2.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
-import yargs from 'yargs'
+import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import wordCount from 'word-count';
 
@@ -34,31 +34,24 @@ const argv = yargs(hideBin(process.argv))
     describe: 'Sustitución global',
     type: 'boolean',
   })
-  .option('r', {
-    alias: 'reverse',
-    describe: 'Comando para revertir el contenido del archivo',
-    type: 'boolean',
-  })
-  .demandCommand(1, 'Debe proporcionar al menos un archivo para procesar.')
+  .demandCommand(2, 'Debe proporcionar al menos un archivo y un comando.')
   .help()
   .argv;
 
-const commands = argv._.filter((arg) => typeof arg === 'string' && arg.startsWith('s/'));
-const fileNames = argv._.filter((arg) => typeof arg === 'string' && !arg.startsWith('s/'));
-const { inplace, silent, scriptFile, print, global, reverse } = argv;
+const [command, fileName] = argv._;
+const { inplace, silent, scriptFile, print, global } = argv;
 
+// Función principal
 async function main() {
-  if (fileNames.length === 0) {
-    console.log('Nombre de archivo no proporcionado.');
-    return;
-  }
-
-  for (const fileName of fileNames) {
-    await processFile(fileName);
+  try {
+    await processFile(fileName, command);
+  } catch (error) {
+    console.log(error.message);
   }
 }
 
-async function processFile(fileName) {
+// Procesar archivo
+async function processFile(fileName, selectedCommand) {
   try {
     await fs.access(fileName);
   } catch {
@@ -66,109 +59,79 @@ async function processFile(fileName) {
     return;
   }
 
-  if (inplace) {
-    const extension = inplace;
-    try {
-      await fs.copyFile(fileName, `${fileName}.${extension}`);
-    } catch (err) {
-      console.log(`Error al hacer una copia del archivo: ${err}`);
-      return;
-    }
+  if (selectedCommand === 'reverse') {
+    await reverseContent(fileName);
+  } else if (selectedCommand === 'count') {
+    await countWords(fileName);
+  } else if (selectedCommand.startsWith('s/')) {
+    await applySubstitution(fileName, selectedCommand);
+  } else {
+    console.log(`Comando no reconocido: ${selectedCommand}`);
   }
+}
 
-  let fileData;
-  try {
-    fileData = await fs.readFile(fileName, 'utf8');
-  } catch (err) {
-    console.log(`Error al leer el archivo: ${err}`);
+// Función para revertir contenido
+async function reverseContent(fileName) {
+  const fileData = await readFile(fileName);
+  const reversedContent = fileData.split('').reverse().join('');
+  printOutput(reversedContent);
+}
+
+// Función para contar palabras
+async function countWords(fileName) {
+  const fileData = await readFile(fileName);
+  const wordCountResult = wordCount(fileData);
+  printOutput(`Número de palabras en el archivo: ${wordCountResult}`);
+}
+
+// Función para aplicar sustitución
+async function applySubstitution(fileName, selectedCommand) {
+  const fileData = await readFile(fileName);
+
+  const match = selectedCommand.match(/s\/(.*?)\/(.*?)\//);
+  if (!match) {
+    console.log(`Comando de sustitución no válido: ${selectedCommand}`);
     return;
   }
 
-  if (scriptFile) {
-    const scriptFilePath = path.resolve(scriptFile);
-    try {
-      await fs.access(scriptFilePath);
-      const scriptCommands = await readScriptFile(scriptFilePath);
-      commands.push(...scriptCommands);
-    } catch (err) {
-      console.log(`Error al leer el Script: ${err}`);
-      return;
-    }
-  }
+  const [, find, replace] = match;
+  const newResult = fileData.replace(new RegExp(find, global ? 'g' : ''), replace);
 
-  for (const command of commands) {
-    const match = command.match(/s\/(.*?)\/(.*?)\//);
-    if (!match) {
-      console.log(`Comando de sustitución no válido: ${command}`);
-      continue;
-    }
-  
-    const [, find, replace] = match;
-    const newResult = fileData.replace(new RegExp(find, global ? 'g' : ''), replace);
-  
-    if (!silent || print) {
-      console.log(newResult);
-    }
-    fileData = newResult;
-  }
+  printOutput(newResult);
 
-  if (argv.count) {
-    countWords(fileData);
-  }
-
-  if (reverse) {
-    reverseContent(fileData);
-  }
-  
   if (inplace) {
-    try {
-      await fs.writeFile(fileName, fileData);
-      console.log(`Se escribió con éxito en el archivo: ${fileName}`);
-    } catch (err) {
-      throw new Error(`Error al escribir en el archivo: ${err}`);
-    }
+    await writeFile(fileName, newResult);
+    console.log(`Se escribió con éxito en el archivo: ${fileName}`);
   }
 }
 
-function countWords(text) {
-  const wordCountResult = wordCount(text);
-  if (!silent || print) {
-    console.log(`Número de palabras en el archivo: ${wordCountResult}`);
-  }
-}
-
-function reverseContent(text) {
-  const reversedContent = text.split('').reverse().join('');
-  if (!silent || print) {
-    console.log(reversedContent);
-  }
-}
-
-async function readScriptFile(scriptFilePath) {
-  const scriptCommands = [];
-  const rl = readline.createInterface({
-    input: await fs.readFile(scriptFilePath, 'utf8')
-  });
-
-  for await (const line of rl) {
-    if (line.startsWith('s/')) {
-      scriptCommands.push(line);
-    }
-  }
-
-  return scriptCommands;
-}
-
-async function processDirectory(directoryPath) {
+// Función para leer archivo
+async function readFile(fileName) {
   try {
-    const files = await fs.readdir(directoryPath);
-    for (const file of files) {
-      const filePath = path.join(directoryPath, file);
-      await processFile(filePath);
-    }
+    const fileData = await fs.readFile(fileName, 'utf8');
+    return fileData;
   } catch (err) {
-    console.log(`Error al leer el directorio: ${err}`);
+    console.log(`Error al leer el archivo: ${err}`);
+    throw err;
   }
 }
 
-main().catch(error => console.log(error.message));
+// Función para escribir archivo
+async function writeFile(fileName, content) {
+  try {
+    await fs.writeFile(fileName, content);
+  } catch (err) {
+    console.log(`Error al escribir en el archivo: ${err}`);
+    throw err;
+  }
+}
+
+// Función para imprimir salida
+function printOutput(output) {
+  if (!silent || print) {
+    console.log(output);
+  }
+}
+
+// Ejecutar función principal
+main();
